ee6d5e494604e952263b39217ecef6bb84366719
EC2 Default User <ec2-user@ip-172-31-33-58.eu-north-1.compute.internal>
2024-12-12 05:04:14 +0000

    インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,25 @@
 class UsersController < ApplicationController
- before_action :authenticate_user!
- before_action :ensure_correct_user, only: [:edit,:update,:destroy]
- 
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
-def index
-  @users=User.all
-  @user=current_user
-  @post=Post.new
-end
+  def index
+    @users = User.all
+    @user = current_user
+    @post = Post.new
+  end
 
   def show
-    @user=User.find(params[:id])
-    @posts=@user.posts
-    @post=Post.new
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @post = Post.new
   end
 
   def edit
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
-      @user=User.find(params[:id])
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "プロフィールが更新されました。"
       redirect_to user_path(@user)
@@ -29,26 +28,26 @@ end
     end
   end
 
- def destroy
-   @user=User.find(params[:id])
-   if @user==current_user
+  def destroy
+    @user = User.find(params[:id])
+    if @user == current_user
       @user.destroy
-     flash[:notice] = "アカウントが削除されました。"
-   end
-     redirect_to new_user_registration_path
+      flash[:notice] = "アカウントが削除されました。"
+    end
+    redirect_to new_user_registration_path
   end
- end
-  
- private
- 
- def user_params
- params.require(:user).permit(:name,:introduction,:profile_image)
- end
- 
- 
- def ensure_correct_user
-   @user=User.find(params[:id])
-   unless @user==current_user
-   redirect_to user_path(current_user)
- end
- end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
+
