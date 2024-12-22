2515fe2c805ec6ac37bcfb7f07531ff18c23542b
EC2 Default User <ec2-user@ip-172-31-33-58.eu-north-1.compute.internal>
2024-12-22 05:14:22 +0000

    コードの修正です

--- db/migrate/20241218053147_rename_book_id_to_post_id_in_favorites.rb
+++ db/migrate/20241218053147_rename_book_id_to_post_id_in_favorites.rb
@@ -1,5 +1,7 @@
 class RenameBookIdToPostIdInFavorites < ActiveRecord::Migration[6.1]
   def change
+    if column_exists?(:favorites, :book_id) && !column_exists?(:favorites, :post_id)
     rename_column :favorites, :book_id, :post_id
+    end
   end
 end
