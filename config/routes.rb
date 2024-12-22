cab6a4d8761e2cf1ec11377519f6d853dc478494
EC2 Default User <ec2-user@ip-172-31-33-58.eu-north-1.compute.internal>
2024-12-22 10:59:14 +0000

    管理者ダッシュボードに削除アクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,6 @@ Rails.application.routes.draw do
   }
   
   namespace :admin  do
-    resources :dashboard, only: [:index, :show] 
+    resources :dashboard, only: [:index, :show, :destroy] 
   end
 end
